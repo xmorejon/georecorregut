53e27433fc36b05fc5cc68797291b6345da7032d
xmorejon <xavier@morejon.cat>
2025-08-31 12:22:10 +0000

    github update

--- src/ai/dev.ts
+++
@@ -1,2 +0,0 @@
-// Flows will be imported for their side effects in this file.
-import './flows/places-flow';
--- src/types/env.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string;
-    NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID: string;
-  }
-}
