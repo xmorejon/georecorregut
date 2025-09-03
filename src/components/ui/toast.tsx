c6ad52ed8d2f3fe00038b5046eb4a951f7bc07b2
xmorejon <xavier@morejon.cat>
2025-09-03 22:01:10 +0000

    move toast for mobile version

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -16,7 +16,9 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      // --- Modified classes for bottom-centered mobile toasts ---
+      "fixed bottom-0 left-1/2 transform -translate-x-1/2 z-[100] flex max-h-screen w-full max-w-sm flex-col-reverse p-4", // Default (mobile) position and styling
+      "sm:bottom-0 sm:left-auto sm:right-0 sm:top-auto sm:flex-col sm:max-w-md", // Small screens and up (adjust breakpoint as needed, bottom-right)
       className
     )}
     {...props}
